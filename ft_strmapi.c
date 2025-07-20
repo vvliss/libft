bd16e2951861529963e04340d0db7309897d4881
vvliss <lisson.wiki@gmail.com>
2025-07-20 22:54:11 +0200

    20

---
+++ ft_strmapi.c
@@ -0,0 +1,21 @@
+#include "libft.h"
+
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+    char    *new_str;
+    size_t  i;
+
+    if (!s || !f)
+        return (NULL);
+    new_str = malloc(strlen(s) + 1);
+    if (!new_str)
+        return (NULL);
+    i = 0;
+    while (s[i])
+    {
+        new_str[i] = f(i, s[i]);
+        i++;
+    }
+    new_str[i] = '\0';
+    return (new_str);
+}
